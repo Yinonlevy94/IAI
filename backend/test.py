f2489214a24a3b987ed8d8d5643877457ec0b372
Yinonlevy94 <91956854+Yinonlevy94@users.noreply.github.com>
2025-11-02 00:01:34 +0200

    Update test.py

--- backend/test.py
+++ backend/test.py
@@ -36,14 +36,6 @@ class TestListUsers:
         assert 'users' in data
         assert 'total' in data
         assert len(data['users']) == 21
-    
-    def test_email_not_exposed(self, client):
-        """emails should never be in the response"""
-        response = client.get('/api/users')
-        users = response.json['users']
-        
-        for user in users:
-            assert 'email' not in user
 
 
 class TestSearch:
